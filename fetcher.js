81c9565b18426d04c2da6e582697399ec2410bec
DLindeblom <drdlindeblom@gmail.com>
2022-06-09 01:12:05 +0000

    fetching and printin

---
+++ fetcher.js
@@ -0,0 +1,22 @@
+const args = process.argv.slice(2);
+const fs = require("fs");
+
+let URL = args[0]
+
+let write = args[1]
+
+const request = require('request');
+request(URL, (error, response, body) => {
+
+  fs.writeFile(write, body, err => {
+    if (err) {
+      console.error(err);
+    }
+    // file written successfully
+    console.log(`Downloaded and saved ${(body.length * 1.5)} bytes to ${write}.`)
+  });
+
+});
+
+
+
